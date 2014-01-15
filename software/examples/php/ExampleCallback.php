1e058651162aa7881a70daf59f554bf44fdcf8de
Matthias Bolte <matthias@tinkerforge.com>
2014-01-15 11:25:41 +0100

    php: Use const for constant values

--- software/examples/php/ExampleCallback.php
+++ software/examples/php/ExampleCallback.php
@@ -6,34 +6,34 @@ require_once('Tinkerforge/BrickletMultiTouch.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickletMultiTouch;
 
-$host = 'localhost';
-$port = 4223;
-$uid = 'XYZ'; // Change to your UID
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = 'XYZ'; // Change to your UID
 
 // Callback function for touch state
 function cb_touch_state($touchState)
 {
-	if($touchState & (1 << 12)) {
-		echo "In proximity, ";
-	}
+    if($touchState & (1 << 12)) {
+        echo "In proximity, ";
+    }
 
-	if(($touchState & 0xfff) == 0) {
-		echo "No electrodes touched\n\n";
-	} else {
-		echo "Electrodes ";
-		for($i = 0; $i < 12; $i++) {
-			if($touchState & (1 << $i)) {
-				echo $i . " ";
-			}
-		}
-		echo "touched\n\n";
-	}
+    if(($touchState & 0xfff) == 0) {
+        echo "No electrodes touched\n\n";
+    } else {
+        echo "Electrodes ";
+        for($i = 0; $i < 12; $i++) {
+            if($touchState & (1 << $i)) {
+                echo $i . " ";
+            }
+        }
+        echo "touched\n\n";
+    }
 }
 
 $ipcon = new IPConnection(); // Create IP connection
-$mt = new BrickletMultiTouch($uid, $ipcon); // Create device object
+$mt = new BrickletMultiTouch(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
 // Register touch state callback to function cb_touch_state
