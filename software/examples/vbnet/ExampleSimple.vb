16efc0fdc73b97e258523ed570d52c9be9ee5ac3
Matthias Bolte <matthias@tinkerforge.com>
2015-08-18 15:59:49 +0200

    Cleanup VB.NET examples

--- software/examples/vbnet/ExampleSimple.vb
+++ software/examples/vbnet/ExampleSimple.vb
@@ -7,15 +7,14 @@ Module ExampleSimple
 
     Sub Main()
         Dim ipcon As New IPConnection() ' Create IP connection
-        Dim si As New BrickletMultiTouch(UID, ipcon) ' Create device object
+        Dim mt As New BrickletMultiTouch(UID, ipcon) ' Create device object
 
         ipcon.Connect(HOST, PORT) ' Connect to brickd
         ' Don't use device before ipcon is connected
 
-        ' Get current touch_state
-        Dim touch_state As Integer = si.GetTouchState()
-
-        System.Console.WriteLine("TouchState: " + touch_state.ToString())
+        ' Get current touch state
+        Dim touchState As Integer = mt.GetTouchState()
+        System.Console.WriteLine("TouchState: " + touchState.ToString())
 
         System.Console.WriteLine("Press key to exit")
         System.Console.ReadLine()
