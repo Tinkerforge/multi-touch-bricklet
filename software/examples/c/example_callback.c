cc6735070d233bb345328c7fcad12d95d3384e35
Matthias Bolte <matthias@tinkerforge.com>
2016-05-18 10:49:54 +0200

    c: Add missing device destroy call

--- software/examples/c/example_callback.c
+++ software/examples/c/example_callback.c
@@ -53,6 +53,7 @@ int main(void) {
 
 	printf("Press key to exit\n");
 	getchar();
+	multi_touch_destroy(&mt);
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
 	return 0;
 }
--- software/examples/c/example_simple.c
+++ software/examples/c/example_simple.c
@@ -49,6 +49,7 @@ int main(void) {
 
 	printf("Press key to exit\n");
 	getchar();
+	multi_touch_destroy(&mt);
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
 	return 0;
 }
