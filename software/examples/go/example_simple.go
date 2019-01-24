37fb31d20afe6ffbc23dd481bb70aac97bfdb72e
Erik Fleckstein <erik@tinkerforge.com>
2019-01-24 14:39:15 +0100

    go: Minor example fixes.

--- software/examples/go/example_simple.go
+++ software/examples/go/example_simple.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-    "github.com/Tinkerforge/go-api-bindings/ipconnection"
-    "github.com/Tinkerforge/go-api-bindings/multi_touch_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
+	"github.com/Tinkerforge/go-api-bindings/multi_touch_bricklet"
 )
 
 const ADDR string = "localhost:4223"
@@ -11,35 +11,33 @@ const UID string = "XYZ" // Change XYZ to the UID of your Multi Touch Bricklet.
 
 func main() {
 	ipcon := ipconnection.New()
-    defer ipcon.Close()
+	defer ipcon.Close()
 	mt, _ := multi_touch_bricklet.New(UID, &ipcon) // Create device object.
 
 	ipcon.Connect(ADDR) // Connect to brickd.
-    defer ipcon.Disconnect()
+	defer ipcon.Disconnect()
 	// Don't use device before ipcon is connected.
 
 	// Get current touch state.
 	state, _ := mt.GetTouchState()
-    var str string
-    
-    if (state & (1 << 12)) == (1 << 12) {
-        str += "In proximity, "
-    }
-    
-    if (state & 0xfff) == 0 {
-        str += "No electrodes touched"
-    } else {
-        str += "Electrodes "
-        for i := uint(0); i < 12; i++ {
-            if (state & (1 << i)) == (1 << i) {
-                str += string(i) + " "
-            }
-        }
-        str += "touched"
-    }    
-    
+	var str string
+
+	if (state & (1 << 12)) == (1 << 12) {
+		str += "In proximity, "
+	}
+
+	if (state & 0xfff) == 0 {
+		str += "No electrodes touched"
+	} else {
+		str += "Electrodes "
+		for i := uint(0); i < 12; i++ {
+			if (state & (1 << i)) == (1 << i) {
+				str += string(i) + " "
+			}
+		}
+		str += "touched"
+	}
+
 	fmt.Print("Press enter to exit.")
 	fmt.Scanln()
-
-	ipcon.Disconnect()
 }
